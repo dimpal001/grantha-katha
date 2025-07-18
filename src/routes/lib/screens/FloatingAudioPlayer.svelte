c80051ccf3d22f7e61be46796c345633f301759c
dimpal001 <dimpaldas518@gmail.com>
2025-07-18 22:57:07 +0530

    resolve play/pause button issue

--- src/routes/lib/screens/FloatingAudioPlayer.svelte
+++ src/routes/lib/screens/FloatingAudioPlayer.svelte
@@ -346,6 +346,11 @@
               class="text-white hover:scale-110 transition-transform"
             />
           </button>
+          <!-- preload both icons (hidden) -->
+          <div style="display: none">
+            <Icon icon="mdi:play-circle" />
+            <Icon icon="mdi:pause-circle" />
+          </div>
 
           <button on:click={togglePlay}>
             <Icon
